8c30c3c77c40d9ff997551c50289a67da066c6fb
ehab akl <ehabakl2023@mail.com>
2023-04-02 10:38:42 +0300

    A

---
+++ main.php
@@ -0,0 +1,44 @@
+<?php 
+
+// echo nl2br("hello\n world \n");
+
+// //******************************************
+
+// echo str_replace("omar","ehab","im omar <br>");
+
+// echo str_word_count("Hope you are doning great ")."<br>";
+
+// echo strlen("ehab")."<br>";
+// //************************************************** 
+// foreach ($_SERVER as $key => $value){
+//     echo $key."=>".$value."<br>" ;
+// }
+//************************************************** 
+
+// $numbers= array( 1=>45 , 0=>12 , 3=>25 , 2=>10 );
+// krsort($numbers) ;
+// print_r($numbers)."<br>" ;
+// foreach($numbers as $key => $value){
+//     $sum = array_sum($numbers) ;
+//     $avg = array_sum($numbers)/count($numbers) ;
+// }
+
+// echo "<br> Sum is = ".$sum."<br> Avg is = ".$avg."<br>" ;
+
+
+//*******************************************************
+ 
+// $y = array("Sara"=>31,"John"=>41,"Walaa"=>39,"Ramy"=>40);
+// asort($y); //value asending 
+// print_r($y);
+// echo "<br>";
+// ksort($y);  //key asending
+// print_r($y);
+// echo "<br>";
+// arsort($y);  //value desending 
+// print_r($y);
+// echo "<br>";
+// krsort($y);  //key desending
+// print_r($y);
+// echo "<br>";
+ ?>
